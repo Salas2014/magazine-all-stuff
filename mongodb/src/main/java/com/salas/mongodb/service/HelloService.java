bffd058328c7bafa6ea4a6e8f3fed1cfe338f68c
Vladyslav Hasporian <salasengland33@gmail.com>
2025-09-10 23:46:52 +0300

    [mongo-service]: add and configure gRPC server with test 'hello service'

---
+++ mongodb/src/main/java/com/salas/mongodb/service/HelloService.java
@@ -0,0 +1,22 @@
+package com.salas.mongodb.service;
+
+import com.salas.mongodb.GreeterGrpc;
+import com.salas.mongodb.HelloWorldProto;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class HelloService extends GreeterGrpc.GreeterImplBase {
+
+    @Override
+    public void sayHello(HelloWorldProto.HelloRequest request,
+                         StreamObserver<HelloWorldProto.HelloReply> responseObserver) {
+
+        String message = "Hello " + request.getName() + " " + request.getLastName() + "!";
+        HelloWorldProto.HelloReply reply = HelloWorldProto.HelloReply
+                .newBuilder()
+                .setMessage(message).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
