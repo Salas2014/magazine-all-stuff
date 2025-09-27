45ca87cdb37930bb6a3569ea8e49b561c7f99f22
Vladyslav Hasporian <salasengland33@gmail.com>
2025-09-27 12:59:39 +0300

    [mongo-db]: drop redundant gradle file; create new module; add consul server into docker compose; add discovery service to producer-service module;

---
+++ producer-service/src/test/java/com/salas/producers/ervice/ProducerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salas.producers.ervice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
