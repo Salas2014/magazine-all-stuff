500ef6af484722bd1ac1eef49079875f8bf9e7bd
Vladyslav Hasporian <salasengland33@gmail.com>
2025-10-06 22:00:17 +0300

    [infostructure]: fix

--- producer-service/src/main/java/com/salas/producers/ervice/ProducerServiceApplication.java
+++ producer-service/src/main/java/com/salas/producers/ervice/ProducerServiceApplication.java
@@ -1,13 +1,47 @@
 package com.salas.producers.ervice;
 
+import net.salas.avro.Event;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.UUID;
 
 @SpringBootApplication
-public class ProducerServiceApplication {
+@EnableScheduling
+public class ProducerServiceApplication implements CommandLineRunner {
+
+    private final KafkaTemplate<String, Event> kafkaTemplate;
+    @Value("${event.topic}")
+    private String topic;
+
+    public ProducerServiceApplication(KafkaTemplate<String, Event> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProducerServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Event producer started");
+    }
+
+    @Scheduled(fixedRateString = "${event.generation.interval-ms}")
+    public void sendEvent() {
+        Event event = Event.newBuilder()
+                .setUid(UUID.randomUUID().toString())
+                .setSubject("subject")
+                .setDescription("description")
+                .build();
+
+        kafkaTemplate.send(topic, event.getUid().toString(), event);
+        System.out.println("Sent: " + event);
     }
 
 }
