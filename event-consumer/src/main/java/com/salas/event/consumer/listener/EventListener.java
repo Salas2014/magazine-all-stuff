79f6545e1a1cf26fc52b0887c60572093a2d450a
Vladyslav Hasporian <salasengland33@gmail.com>
2025-10-09 21:36:47 +0300

    [infostructure]: add logstash; add alloy agent and lodi datasource;

--- event-consumer/src/main/java/com/salas/event/consumer/listener/EventListener.java
+++ event-consumer/src/main/java/com/salas/event/consumer/listener/EventListener.java
@@ -3,6 +3,8 @@ package com.salas.event.consumer.listener;
 import com.salas.event.consumer.module.EventEntity;
 import com.salas.event.consumer.repository.EventRepository;
 import net.salas.avro.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Component;
 public class EventListener {
 
     private final EventRepository repository;
+    private final Logger logger = LoggerFactory.getLogger(EventListener.class);
 
     public EventListener(EventRepository repository) {
         this.repository = repository;
@@ -17,11 +20,13 @@ public class EventListener {
 
     @KafkaListener(topics = "events", groupId = "event-group")
     public void listen(Event event) {
+        logger.info("Received event {}", event);
         EventEntity entity = new EventEntity();
         entity.setUid(event.getUid());
         entity.setSubject(event.getSubject());
         entity.setDescription(event.getDescription());
         repository.save(entity);
-        System.out.println("Saved: " + entity);
+
+        logger.info("Saved event {}", entity);
     }
 }
