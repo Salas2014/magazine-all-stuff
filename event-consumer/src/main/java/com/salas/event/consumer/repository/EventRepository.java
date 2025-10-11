bad1b66f6f639df89c6a34eb94ef3e149ea0dd55
Vladyslav Hasporian <salasengland33@gmail.com>
2025-10-11 16:45:45 +0300

    [infostructure]: refactor event consumer

--- event-consumer/src/main/java/com/salas/event/consumer/repository/EventRepository.java
+++ event-consumer/src/main/java/com/salas/event/consumer/repository/EventRepository.java
@@ -3,5 +3,5 @@ package com.salas.event.consumer.repository;
 import com.salas.event.consumer.module.EventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventRepository extends JpaRepository<EventEntity, Long> {
+public interface EventRepository extends JpaRepository<EventEntity, String> {
 }
