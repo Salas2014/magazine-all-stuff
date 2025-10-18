cfe8a807e5d6133ad738fe046dcfe6ed130d30d3
Vladyslav Hasporian <salasengland33@gmail.com>
2025-10-18 21:34:04 +0300

    [terraform-aws-provider]: add vpc with 2 public subnets

--- terraform/V1-EC2.tf
+++ terraform/V1-EC2.tf
@@ -15,11 +15,13 @@ resource "aws_instance" "demo-server" {
   instance_type = "t2.micro"
   key_name = "keyPairDemo"
   vpc_security_group_ids = [aws_security_group.demo-sg.id]
+  subnet_id = aws_subnet.dpw-public_subent_01.id
 }
 
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
   description = "SSH Access"
+  vpc_id = aws_vpc.dpw-vpc.id
 
   ingress {
     description = "SSH Access"
@@ -40,3 +42,60 @@ resource "aws_security_group" "demo-sg" {
     Name = "ssh-port"
   }
 }
+
+
+resource "aws_vpc" "dpw-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "dpw-vpc"
+  }
+}
+
+resource "aws_subnet" "dpw-public_subent_01" {
+  vpc_id                  = aws_vpc.dpw-vpc.id
+  cidr_block              = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "dpw-public_subent_01"
+  }
+}
+
+resource "aws_subnet" "dpw-public_subent_02" {
+  vpc_id                  = aws_vpc.dpw-vpc.id
+  cidr_block              = "10.1.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "dpw-public_subent_02"
+  }
+}
+
+resource "aws_internet_gateway" "dpw-igw" {
+  vpc_id = aws_vpc.dpw-vpc.id
+  tags = {
+    Name = "dpw-igw"
+  }
+}
+
+resource "aws_route_table" "dpw-public-rt" {
+  vpc_id = aws_vpc.dpw-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dpw-igw.id
+  }
+  tags = {
+    Name = "dpw-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "dpw-rta-public-subent-1" {
+  subnet_id      = aws_subnet.dpw-public_subent_01.id
+  route_table_id = aws_route_table.dpw-public-rt.id
+}
+
+resource "aws_route_table_association" "dpw-rta-public-subent-2" {
+  subnet_id      = aws_subnet.dpw-public_subent_02.id
+  route_table_id = aws_route_table.dpw-public-rt.id
+}
+
