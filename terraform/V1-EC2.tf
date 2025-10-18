1f80a0af283b34e8a8bbf0b26042199280065f17
Vladyslav Hasporian <salasengland33@gmail.com>
2025-10-18 21:00:15 +0300

    [terraform-aws-provider]: add securite froup into tr file

--- terraform/V1-EC2.tf
+++ terraform/V1-EC2.tf
@@ -14,4 +14,29 @@ resource "aws_instance" "demo-server" {
   ami = "ami-0341d95f75f311023"
   instance_type = "t2.micro"
   key_name = "keyPairDemo"
+  vpc_security_group_ids = [aws_security_group.demo-sg.id]
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+
+  ingress {
+    description = "SSH Access"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+  }
 }
